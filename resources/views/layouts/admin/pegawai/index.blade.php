fb7e591a06965e42ba125088658df7592ed071c8
TheGenesis7 <thegenesispath@gmail.com>
2018-10-31 19:07:04 +0800

    Merge branch 'devel' of http://gitlab.docotel.net/dirham/e-kinerja into devel
    # Conflicts:
    #	app/Http/Controllers/MasterData/PegawaiController.php
    #	resources/views/layouts/admin/pegawai/index.blade.php
    #	resources/views/layouts/partial/part/ajax.blade.php

--- resources/views/layouts/admin/pegawai/index.blade.php
+++ resources/views/layouts/admin/pegawai/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.partial.main')
+@extends('layouts.admin.partial.main')
 @section('content')
   <div class="main">
       <div class="nav-top-container">
@@ -24,7 +24,52 @@
               <tbody class="list_pegawai">
               </tbody>
           </table>
+            <div class="row text-center">
+                <ul class="pagination" id="pagination"></ul>
+            </div>
         </div>
     </div>
   </div>
+    @push('script')
+            <script>
+                $(document).ready(function(){
+                    getPage();
+                    getData(1);
+                });
+                var getPage = function () {
+                    $('#pagination').twbsPagination('destroy');
+                    $.get('{{route('page_pegawai')}}')
+                        .then(function (res) {
+                            $('#pagination').twbsPagination({
+                                totalPages: res.halaman,
+                                visiblePages: 5,
+                                onPageClick: function (event, page) {
+                                    getData(page);
+                                }
+                            });
+                        })
+                };
+                var getData = function (page) {
+                    var listArr = [];
+                    var row = '';
+                    var selector = $('.list_pegawai');
+                    $.ajax({
+                        url: "{{ route('list_pegawai') }}?page="+page,
+                        data: '',
+                        success: function(res){
+                            for(i = 0; i < res.response.data.length; i++) {
+                                row += "<tr>";
+                                row += "<td><div class='img-user' id='user1' style='background-image: url({{ asset('assets/images/img-user.png') }});'></div></td>";
+                                row += "<td>"+res.response.data[i].nip+"</td>";
+                                row += "<td>"+res.response.data[i].nama+"</td>";
+                                row += "<td>"+res.response.data[i].jabatan.jabatan+"</td>";
+                                row += "<td>"+res.response.data[i].jns_kel+"</td></tr>";
+                                row += "</tr>";
+                            }
+                            selector.html(row);
+                        }
+                    });
+                }
+            </script>
+    @endpush
 @endsection
