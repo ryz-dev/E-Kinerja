af38891a2bf581336e4d4c984a679faf95522fb2
TheGenesis7 <thegenesispath@gmail.com>
2018-11-02 13:41:42 +0800

    sidebar class active

--- resources/views/layouts/admin/partial/part/sidebar.blade.php
+++ resources/views/layouts/admin/partial/part/sidebar.blade.php
@@ -3,10 +3,10 @@
         <img src="{{ asset('assets/images/logo.svg') }}" width="120">
     </div>
     <ul>
-        <li><a href="{{route('pegawai.index')}}" class="{{url()->current() == route('pegawai.index') ? 'active' : ''}}">Pegawai</a></li>
-        <li><a href="{{route('hari_kerja')}}" class="{{url()->current() == route('hari_kerja') ? 'active' : ''}}">Hari Kerja</a></li>
-        <li><a href="{{route('eselon.index')}}" class="{{url()->current() == route('eselon.index') ? 'active' : ''}}">Eselon</a></li>
-        <li><a href="{{route('jabatan.index')}}" class="{{url()->current() == route('jabatan.index') ? 'active' : ''}}">Jabatan</a></li>
+        <li><a href="{{route('pegawai.index')}}" class="{{str_contains(url()->current(),route('pegawai.index')) ? 'active' : ''}}">Pegawai</a></li>
+        <li><a href="{{route('hari_kerja')}}" class="{{str_contains(url()->current(),route('hari_kerja')) ? 'active' : ''}}">Hari Kerja</a></li>
+        <li><a href="{{route('eselon.index')}}" class="{{str_contains(url()->current(),route('eselon.index')) ? 'active' : ''}}">Eselon</a></li>
+        <li><a href="{{route('jabatan.index')}}" class="{{str_contains(url()->current(),route('jabatan.index')) ? 'active' : ''}}">Jabatan</a></li>
         {{--<li><a href="penilaian-etika.html">Penilaian Etika</a></li>--}}
         {{--<li><a href="tunjangan-kinerja.html">Tunjangan Kinerja</a></li>--}}
         @stack('sidebar')
