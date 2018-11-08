c2af21d71ba7fb5b927c94c6fc5730359c094850
mzulkifli <muh.zulkifli@docotel.com>
2018-11-08 13:45:19 +0800

    + fungsi pencarian pegawai

--- app/Http/Controllers/API/MonitoringAbsenController.php
+++ app/Http/Controllers/API/MonitoringAbsenController.php
@@ -14,11 +14,15 @@ class MonitoringAbsenController extends Controller
         $this->show_limit = $request->has('s') ? $request->input('s') : $this->show_limit;
         $skpd = $request->input('skpd');
         $date = \Carbon\Carbon::parse($request->input('d'));
+        $search = $request->has('search')? $request->input('search'):'';
+
+        // wherehas('jabatan', function($query){ 
+        //     $query->where('id_atasan','=',2 /** TODO : Ganti dengan user yang login */ ); })
+        // ->
+
         try {
             if ($skpd == 0) {
-                $pegawai = Pegawai::wherehas('jabatan', function($query){ 
-                                            $query->where('id_atasan','=',2 /** TODO : Ganti dengan user yang login */ ); })
-                                        ->with(['checkinout' => function($query) use ($date){
+                $pegawai = Pegawai::with(['checkinout' => function($query) use ($date){
                                                 $query->select('userid','checktime','checktype')
                                                       ->whereDate('checktime','=',$date)->get();
                                                 }])
@@ -31,16 +35,23 @@ class MonitoringAbsenController extends Controller
                                                     }])
                                             ->orderBy('nama','asc');
             }
+
+            if ($search) {
+                $pegawai->where(function($query) use ($search){
+                    $query->where('nip','like','%'.$search.'%')->orWhere('nama','like','%'.$search.'%');
+                });
+            }
             
             $pegawai = $pegawai->paginate($this->show_limit);
-
+            $queries = \DB::getQueryLog();
+            
             return $this->ApiSpecResponses(
                 [
                     'pegawai' => $pegawai,
                     'dayBefore' => Carbon::parse($date)->addDays(-1)->format('m/d/Y'),
                     'dayAfter' => Carbon::parse($date)->addDays(1)->format('m/d/Y'),
                     'today' => Carbon::parse($date)->format('m/d/Y'),
-                    'dateString' => Carbon::parse($date)->format('d M Y')
+                    'dateString' => Carbon::parse($date)->format('d F Y')
                 ]
             );
         } catch (Exception $e) {
@@ -52,16 +63,25 @@ class MonitoringAbsenController extends Controller
 
     public function getPage(Request $request){
         $skpd = $request->input('skpd');
+        $search = $request->has('search')?$request->input('search'):'';
+
         if ($skpd==0) {
-            $data = Pegawai::wherehas('jabatan', function($query){
-                $query->where('id_atasan','=',2 /** TODO : Ganti dengan user yang login */);
-            })->count();    
+            $data = new Pegawai();// wherehas('jabatan', function($query){
+            //     $query->where('id_atasan','=',2 /** TODO : Ganti dengan user yang login */);
+            // dd($data);
+            // });    
         }else{
-            $data = Pegawai::where('id_skpd',$skpd)->count();
+            $data = Pegawai::where('id_skpd',$skpd);
         }
+
+        if ($search) {
+            $data->where(function($query) use ($search){
+                $query->where('nip','like','%'.$search.'%')->orWhere('nama','like','%'.$search.'%');
+            });
+        }
+
         
-        
-        $data = ceil($data / $this->show_limit);
+        $data = ceil($data->count() / $this->show_limit);
 
         return response()->json([ 'page'=> $data ]);
     }
