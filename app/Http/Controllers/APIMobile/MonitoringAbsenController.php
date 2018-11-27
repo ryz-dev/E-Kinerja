4443bc977baa4f25b2a9de09572581ed08ddd7d2
Hury <mashurimansur18@gmail.com>
2018-11-27 18:48:16 +0800

    perbaikan kinerja di monitoring api mobile

--- app/Http/Controllers/APIMobile/MonitoringAbsenController.php
+++ app/Http/Controllers/APIMobile/MonitoringAbsenController.php
@@ -78,7 +78,7 @@ class MonitoringAbsenController extends Controller
                         'in' => (count($p->checkinout) != null) ? $p->checkinout[0]->checktime : null,
                         'out' => (count($p->checkinout) > 1) ? $p->checkinout[1]->checktime : null,
                     ],
-                    'kinerja' => $p->kinerja,
+                    'kinerja' => (count($p->kinerja) != null) ? $p->kinerja[0]->jenis_kinerja : null,
                 ];
             }
 
