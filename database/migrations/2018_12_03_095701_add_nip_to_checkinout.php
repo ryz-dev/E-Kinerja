caaaaa1847fa492406b54733692ac8dd66b06d8e
Dirham <dirhamsistem@gmail.com>
2018-12-03 19:45:17 +0800

    fix error migration checkinout

--- database/migrations/2018_12_03_095701_add_nip_to_checkinout.php
+++ database/migrations/2018_12_03_095701_add_nip_to_checkinout.php
@@ -14,7 +14,7 @@ class AddNipToCheckinout extends Migration
     public function up()
     {
         Schema::table('checkinout', function (Blueprint $table) {
-            $table->string('nip')->after('userid');
+            $table->string('nip');
         });
     }
 
