9e4f3d703b0cf91a576353fc63e0acfb3ff98a7a
Hury <mashurimansur18@gmail.com>
2018-12-13 14:39:00 +0800

    perbaikan pada rekap dan kinerja api mobile

--- app/Http/Controllers/APIMobile/RekapBulananController.php
+++ app/Http/Controllers/APIMobile/RekapBulananController.php
@@ -174,8 +174,8 @@ class RekapBulananController extends ApiController
             'nama' => $pegawai->nama,
             'nip' => $pegawai->nip,
             'foto' => $pegawai->foto,
-            'kinerja' => $kinerja ? $kinerja : null,
-            'etika' => $etika ? $etika : null,
+            'kinerja' => $kinerja,
+            'etika' => $etika,
             'checkinout' => [
                 'in' => (count($checkinout)) ? $checkinout[0]->checktime : "",
                 'out' => (count($checkinout) > 1) ? $checkinout[1]->checktime : "",
