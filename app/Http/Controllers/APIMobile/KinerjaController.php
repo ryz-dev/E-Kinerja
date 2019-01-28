f7a2057877cfb3b614634071c17c4d76e01933d7
Hury <mashurimansur18@gmail.com>
2019-01-28 16:02:33 +0800

    update kinerja

--- app/Http/Controllers/APIMobile/KinerjaController.php
+++ app/Http/Controllers/APIMobile/KinerjaController.php
@@ -212,7 +212,7 @@ class KinerjaController extends ApiController
                 $knj = Kinerja::where('nip', $nip)->where('tgl_mulai', '<=', $hk->tanggal)->where('tgl_selesai', '>=', $hk->tanggal)->where('approve', 2)->terbaru();
                 // $etk = Etika::where('nip', $nip)->where('tanggal', '=', $hk->tanggal)->first();
                 $abs = Checkinout::where('nip', $nip)->whereDate('checktime', $hk->tanggal)->get();
-                $status = $knj->first() ? $knj->first()->jenis_kinerja : 'alpa';                
+                $status = $knj->first() ? $knj->first()->jenis_kinerja : '';                
                 // $status = '';                
                 if ($abs->count() > 0) {
                     $in = false;
@@ -254,6 +254,7 @@ class KinerjaController extends ApiController
                     'approve' => $knj->first() ? $knj->first()->approve : 0,
                     'etika' => $etika ? $etika->persentase : 0,
                     'status' => $status,
+                    
                 ];
                 // $etika[] = $etk ? $etk->toArray() : null;
                 // if ($etk) {
