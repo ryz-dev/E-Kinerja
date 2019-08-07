be817c3736c6b011b78915eb7361d5b5d463024d
zulkifli_r <muh.zulkifli@docotel.com>
2019-08-07 19:56:14 +0800

    sort by golongan.tunjangan

--- app/Repositories/PegawaiRepository.php
+++ app/Repositories/PegawaiRepository.php
@@ -857,14 +857,14 @@ class PegawaiRepository extends BaseRepository
             })->select('id')->pluck('id')->all();
             $pegawai->whereNotIn('id_jabatan',$jabatan_hide);
 
-            $pegawai->orderBy('jabatan.tunjangan', 'asc');
-            // $pegawai->orderBy('role_pegawai.id_role','asc');
-            $pegawai->orderBy('pegawai.nama');
-            $data_absen_pegawai = $this->parseAbsensi($pegawai, $date, $status_hari->id_status_hari, $is_mobile)->where('nama','!=','Master Data Admin')->sortBy('role_id')->values();
             $pegawai->orderBy('golongan.tunjangan', 'desc');
-            $pegawai->orderBy('role_pegawai.id_role','asc');
+            // $pegawai->orderBy('role_pegawai.id_role','asc');
             // $pegawai->orderBy('pegawai.nama');
-            $data_absen_pegawai = $this->parseAbsensi($pegawai, $date, $status_hari->id_status_hari, $is_mobile)->where('nama','!=','Master Data Admin')->sortBy('urutan')->values();
+            $data_absen_pegawai = $this->parseAbsensi($pegawai, $date, $status_hari->id_status_hari, $is_mobile)->where('nama','!=','Master Data Admin')->sortBy('role_id')->values();
+            // $pegawai->orderBy('golongan.tunjangan', 'desc');
+            // $pegawai->orderBy('role_pegawai.id_role','asc');
+            // $pegawai->orderBy('pegawai.nama');
+            // $data_absen_pegawai = $this->parseAbsensi($pegawai, $date, $status_hari->id_status_hari, $is_mobile)->where('nama','!=','Master Data Admin')->sortBy('urutan')->values();
 
             $sum = $this->summary($data_absen_pegawai, $raw_date, $status_hari->id_status_hari);
             // dd($data_absen_pegawai);
